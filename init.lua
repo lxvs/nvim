ce42e1bfd402f4b850c7b4b6e481bd55a667fdda
Liu, Zhao-hui <lzhh@duck.com>
2025-11-25 17:54:46 +0800

    Add telescope

--- init.lua
+++ init.lua
@@ -138,4 +138,10 @@ require('ToggleTerm').setup {
   direction = 'float',
 }
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 -- vim: sw=2 sts=2
