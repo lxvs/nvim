cd42b3afca05bb47b58740d219a56c62a5694900
Liu, Zhao-hui <lzhh@duck.com>
2025-12-09 16:37:29 +0800

    Add nvim-cmp

--- init.lua
+++ init.lua
@@ -232,17 +232,6 @@ require('trouble').setup {}
 vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<CR>')
 vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>')
 
-if vim.fn.has('nvim-0.11.0') == 1 then
-  vim.cmd('packadd! nvim-lspconfig')
-  if vim.fn.has('win32') == 1 then
-    vim.lsp.config('bashls', {cmd = {'bash-language-server.cmd', 'start' }})
-    vim.lsp.config('pyright',{cmd = {'pyright-langserver.cmd', '--stdio'}})
-  end
-
-  vim.lsp.enable('bashls')
-  vim.lsp.enable('pyright')
-end
-
 require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
@@ -252,5 +241,68 @@ require('nvim-treesitter.configs').setup {
     enable = true,
   },
 }
+
+do -- nvim-cmp
+  local cmp = require('cmp')
+
+  cmp.setup {
+    snippet = {
+      expand = function(args)
+        vim.fn['vsnip#anonymous'](args.body)
+      end
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources ({
+      {name = 'nvim_lsp'},
+      {name = 'vsnip'},
+    }, {
+      {name = 'buffer'},
+    })
+  }
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
+end
+
+if vim.fn.has('nvim-0.11.0') == 1 then
+  vim.cmd('packadd! nvim-lspconfig')
+  if vim.fn.has('win32') == 1 then
+    vim.lsp.config('bashls', {cmd = {'bash-language-server.cmd', 'start' }})
+    vim.lsp.config('pyright',{cmd = {'pyright-langserver.cmd', '--stdio'}})
+  end
+
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  vim.lsp.config('bashls', {capabilities = capabilities})
+  vim.lsp.config('pyright',{capabilities = capabilities})
+
+  vim.lsp.enable('bashls')
+  vim.lsp.enable('pyright')
+end
 
 -- vim: sw=2 sts=2
