d17029b4c7d13c47628c19306ec604b25669a7ad
Liu, Zhao-hui <lzhh@duck.com>
2025-12-15 14:29:12 +0800

    Disable cmp for `/` and `?` which breaks up/down key

--- init.lua
+++ init.lua
@@ -281,13 +281,13 @@ do -- nvim-cmp
     })
   }
 
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
+--   -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+--   cmp.setup.cmdline({ '/', '?' }, {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = {
+--       { name = 'buffer' }
+--     }
+--   })
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
