b2313e8375b6b28151f73ce74fd45a683082c9ee
Liu, Zhao-hui <lzhh@duck.com>
2025-12-26 21:01:56 +0800

    Add nvim-ts-autotag

--- init.lua
+++ init.lua
@@ -321,4 +321,20 @@ if vim.fn.has('nvim-0.11.0') == 1 then
   vim.keymap.set('n', 'ge', vim.lsp.buf.references)
 end
 
+require('nvim-ts-autotag').setup {
+  opts = {
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = true -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+  -- per_filetype = {
+  --   ["html"] = {
+  --     enable_close = false
+  --   }
+  -- }
+}
+
 -- vim: sw=2 sts=2
