21c8135f581892d4e15f1157abe929dd9060e91c
Liu, Zhao-hui <lzhh@duck.com>
2026-01-06 15:41:33 +0800

    Disable all git-reset key mappings

--- init.lua
+++ init.lua
@@ -195,18 +195,18 @@ require('gitsigns').setup {
 
     -- Actions
     map('n', '<leader>hs', gitsigns.stage_hunk)
-    map('n', '<leader>hr', gitsigns.reset_hunk)
+    -- map('n', '<leader>hr', gitsigns.reset_hunk)
 
     map('v', '<leader>hs', function()
       gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
     end)
 
-    map('v', '<leader>hr', function()
-      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-    end)
+    -- map('v', '<leader>hr', function()
+    --   gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    -- end)
 
     map('n', '<leader>hS', gitsigns.stage_buffer)
-    map('n', '<leader>hR', gitsigns.reset_buffer)
+    -- map('n', '<leader>hR', gitsigns.reset_buffer)
     map('n', '<leader>hp', gitsigns.preview_hunk)
     map('n', '<leader>hi', gitsigns.preview_hunk_inline)
 
